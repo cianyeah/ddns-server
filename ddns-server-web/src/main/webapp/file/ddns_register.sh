5b8f978f3021decfa6e75249d5d3df3da509d9d0
Tianya <tianya@weidian.com>
2017-10-19 09:46:57 +0800

    首页添加示例

--- ddns-server-web/src/main/webapp/file/ddns_register.sh
+++ ddns-server-web/src/main/webapp/file/ddns_register.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# example
-# rm -f /tmp/ddns_register.sh && wget -q -P /tmp "http://centos.aynait.com:48080/ddns-server/file/ddns_register.sh" && chmod 777 /tmp/ddns_register.sh && bash /tmp/ddns_register.sh "domain" &
-
 domain=$1
 token=$(date "+%Y-%m-%d %H:%M:%S")
 token=${token// /%20}
