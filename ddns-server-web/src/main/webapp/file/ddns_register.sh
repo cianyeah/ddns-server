ae3218c7b0979273f898133deabbe5691f41601c
tianya <tianya@aynait.com>
2019-01-31 14:13:45 +0800

    使用Spring http-basic验证

--- ddns-server-web/src/main/webapp/file/ddns_register.sh
+++ ddns-server-web/src/main/webapp/file/ddns_register.sh
@@ -4,4 +4,4 @@ domain=$1
 token=$(date "+%Y-%m-%d %H:%M:%S")
 token=${token// /%20}
 
-curl -s -i -H "Accept: application/json" "http://ddns-server.aynait.com:48080/ddns-server/addDnsARecord?domain=${domain}&token=${token}"
+curl -s -i -H "Accept: application/json" -u admin:/*Tianya*/ "http://ddns-server.aynait.com:48080/ddns-server/addDnsARecord?domain=${domain}&token=${token}"
